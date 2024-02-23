42a36ba6170c8e53222f7835ef499be2cda3049e
schinak-m <shinak.x3@gmail.com>
2024-02-23 22:21:35 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+
+  const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+
+const superbowlWin = (arr) => {
+    const wins = arr.find((element) => {
+        if(element.result === "W"){
+            return element
+        }
+    })
+    if(wins){
+        return wins.year
+    }
+}
+
+console.log(superbowlWin(sadReality))
